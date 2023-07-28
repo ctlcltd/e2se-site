ea7c6041e4cd501d8541c795b91167c22669fa5c
Leonardo Laureti <204957+leolweb@users.noreply.github.com>
2023-07-28 22:39:30 +0200

    js token, noscript message box

--- public/translate/script.js
+++ public/translate/script.js
@@ -623,6 +623,36 @@ function styles() {
 
 styles();
 
+function token() {
+  var w = 10;
+  const a = [
+    [ 48, 57 ],         // 0-9
+    [ 97, 122 ],        // a-z
+    [ 65, 90 ],         // A-Z
+    [ 36, 38, 61, 64 ]  // $,&,=,@
+  ];
+  var s = '';
+
+  while (w--) {
+    let n;
+    let i = Math.floor(Math.random() * 4);
+    if (i == 3 && Math.random() * 100 < 50) {
+      i -= parseInt(Math.random() * 3);
+    }
+    if (i == 3) {
+      n = Math.floor(Math.random() * a[i].length);
+      n = a[i][n];
+    } else {
+      n = Math.floor((Math.random() * (a[i][1] - a[i][0] + 1)) + a[i][0]);
+    }
+    s += String.fromCharCode(n);
+  }
+
+  return s;
+}
+
+window.token = token;
+
 
 function api_request(method, endpoint, body) {
   const xhr = new XMLHttpRequest();
