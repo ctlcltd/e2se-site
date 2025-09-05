b02552c2beab2e17b569281fe06457a978318c3b
Leonardo Laureti <204957+leolweb@users.noreply.github.com>
2025-09-05 18:15:55 +0200

    update site pages

--- src/Gruntfile.js
+++ src/Gruntfile.js
@@ -57,8 +57,8 @@ module.exports = function(grunt) {
             origin: REMOTE_ORIGIN
           },
           data: {
-            site_privacy_rev: new Date('2025-07-17'),
-            software_privacy_rev: new Date('2025-07-17')
+            site_privacy_rev: new Date('2025-09-05'),
+            software_privacy_rev: new Date('2025-09-05')
           }
         },
         src: ['site/liquid/*.liquid'],
